9f5ffc1f3841a2dd44803d08d2fa157c7e0b9212
Bampie <baptist.o@outlook.com>
2022-12-13 15:12:20 +0100

    opgeschoond

--- exec.ps1
+++ exec.ps1
@@ -1,2 +1,2 @@
-docker build --rm -t auth0-vue-01-login .
-docker run -p 3000:3000 --pid=host auth0-vue-01-login
+docker build --rm -t s3-frontend:3.0.0 .
+docker run -p 3000:3000 --pid=host s3-frontend:3.0.0
